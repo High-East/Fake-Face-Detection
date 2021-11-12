d5ebbc5eac4de7bdb3419131ae43e2b2672eae57
high-east <rhehd127@gmail.com>
2021-11-12 20:22:47 +0900

    Add guarantee_numpy

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -5,12 +6,13 @@ import torch.nn.functional as F
 import torchvision.models as models
 from tqdm import tqdm
 
+
 def get_network(
-    options
+        options
 ):
     model = None
 
-    if options.network == "ResNet":        
+    if options.network == "ResNet":
         model = models.resnet18(pretrained=options.model.pretrained)
         model.fc = nn.Linear(512, options.data.num_classes)
 
@@ -19,9 +21,10 @@ def get_network(
 
     return model.to(options.device)
 
+
 def get_optimizer(
-    params,
-    options
+        params,
+        options
 ):
     if options.optimizer.type == "Adam":
         optimizer = optim.Adam(params, lr=options.optimizer.lr, weight_decay=options.optimizer.weight_decay)
@@ -29,3 +32,18 @@ def get_optimizer(
         raise NotImplementedError
 
     return optimizer
+
+
+def guarantee_numpy(data):
+    data_type = type(data)
+    if data_type == torch.Tensor:
+        device = data.device.type
+        if device == 'cpu':
+            data = data.detach().numpy()
+        else:
+            data = data.detach().cpu().numpy()
+        return data
+    elif data_type == np.ndarray or data_type == list:
+        return data
+    else:
+        raise ValueError("Check your data type.")
